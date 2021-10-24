8d18fb409059a536f2790af47f83e29814c21025
a1mond <kotik.slava2012@gmail.com>
2021-10-24 12:55:40 +0200

    added new TextScreen screen

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from 'react-native';
+
+const TextScreen = () => {
+    return <View>
+
+    </View>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default TextScreen;
